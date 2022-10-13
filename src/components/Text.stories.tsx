084d6d6fea35725f2b2c49e72e1d8b0099a61743
Juliao96 <julio.brito@allin.com.br>
2022-10-13 17:43:08 -0300

    add Text component

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Text, TextProps } from "./Text";
+
+export default {
+    title: 'Component/Text',
+    component: Text,
+    args: {
+        children: 'Mussum Ipsum.',
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p> Teste </p>
+        )
+    }
+}
