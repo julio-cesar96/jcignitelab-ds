cfcd7c867d406eb739c8c48ea2ef0ba4d6bd07c9
Juliao96 <julio.brito@allin.com.br>
2022-10-14 12:39:28 -0300

    add Checkbox component

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -10,10 +10,16 @@ export default {
             <TextInput.Icon>
                 <Envelope />
             </TextInput.Icon>,
-             <TextInput.Input placeholder='Digite seu e-mail' />
+            <TextInput.Input placeholder='Digite seu e-mail' />
         ],
     },
     argTypes: {}
 } as Meta<TextInputRootProps>
 
 export const Default: StoryObj<TextInputRootProps> = {}
+
+export const InputWithoutIcon: StoryObj<TextInputRootProps> = {
+    args: {
+        children: <TextInput.Input placeholder='Digite seu e-mail' /> 
+    }
+}
